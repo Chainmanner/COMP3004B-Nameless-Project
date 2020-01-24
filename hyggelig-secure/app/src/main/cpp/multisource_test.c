c88fb64902aab33487164afd6c440351e6081dba
Gabriel Valachi <pompilica2000@yahoo.com>
2020-01-23 20:49:12 -0500

    Added the Android Studio project. More details coming soon.

---
+++ hyggelig-secure/app/src/main/cpp/multisource_test.c
@@ -0,0 +1,10 @@
+//
+// Created by chainmanner on 1/23/20.
+//
+
+#include "multisource_test.h"
+
+const char* getTheTestString( void )
+{
+    return "Oh no! No, not the bees! NOT THE BEES! AAAAAAAAAAAAAAAAAAAAAAAAAAAAA['A' repeats infinitely many times]";
+}
---
+++ hyggelig-secure/app/src/main/cpp/multisource_test.h
@@ -0,0 +1,12 @@
+//
+// Created by chainmanner on 1/23/20.
+//
+
+#ifndef HYGGELIG_SECURE_MULTISOURCE_TEST_H
+#define HYGGELIG_SECURE_MULTISOURCE_TEST_H
+
+#include <string.h>
+
+const char* getTheTestString( void );
+
+#endif //HYGGELIG_SECURE_MULTISOURCE_TEST_H
