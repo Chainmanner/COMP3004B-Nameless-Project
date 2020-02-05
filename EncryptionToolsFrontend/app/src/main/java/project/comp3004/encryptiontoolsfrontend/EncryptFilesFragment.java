f762efbc5ccd968ff1f9ca4d1ff9c53ba4e75531
Gabriel Valachi <pompilica2000@yahoo.com>
2020-02-04 21:01:22 -0500

    Gabriel: Added just a little more functionality to the Encryption Tools front end, but not much. Nothing to note here.

--- EncryptionToolsFrontend/app/src/main/java/project/comp3004/encryptiontoolsfrontend/EncryptFilesFragment.java
+++ EncryptionToolsFrontend/app/src/main/java/project/comp3004/encryptiontoolsfrontend/EncryptFilesFragment.java
@@ -1,6 +1,35 @@
 package project.comp3004.encryptiontoolsfrontend;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 public class EncryptFilesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View theView = inflater.inflate(R.layout.encrypt_files_layout, container, false);
+
+        // This adds a back button.
+        Toolbar toolbar = theView.findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        if ( ((MainActivity)getActivity()).getSupportActionBar() != null )
+        {
+            ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            ((MainActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   // This actually makes the back button go back.
+                Navigation.findNavController(v).navigate(R.id.action_encryptFilesFragment2_to_encryptionToolsMainMenuFragment3);
+            }
+        });
+
+        return theView;
+    }
 }
--- EncryptionToolsFrontend/app/src/main/java/project/comp3004/encryptiontoolsfrontend/EncryptionToolsMainMenuFragment.java
+++ EncryptionToolsFrontend/app/src/main/java/project/comp3004/encryptiontoolsfrontend/EncryptionToolsMainMenuFragment.java
@@ -1,6 +1,47 @@
 package project.comp3004.encryptiontoolsfrontend;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class EncryptionToolsMainMenuFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // NOTE: The "false" here is VERY important.
+        View theView = inflater.inflate(R.layout.main, container, false);
+
+        // TODO: All buttons must be clickable.
+
+        theView.findViewById(R.id.encryptFiles).setOnClickListener( new View.OnClickListener()
+                                                                    {
+                                                                        @Override
+                                                                        public void onClick(View v)
+                                                                        {
+                                                                            Snackbar.make(v, "wow", Snackbar.LENGTH_SHORT).show();
+                                                                            Navigation.findNavController(v).navigate(R.id.action_encryptionToolsMainMenuFragment3_to_encryptFilesFragment2);
+                                                                        }
+                                                                    }
+        );
+
+        // This adds a back button.
+        Toolbar toolbar = theView.findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        if ( ((MainActivity)getActivity()).getSupportActionBar() != null )
+        {
+            ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            ((MainActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        // Will add code to make the back button go back when integrating this with the rest of the project.
+
+        return theView;
+    }
 }
